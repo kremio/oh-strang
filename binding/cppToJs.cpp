e3f0b7ea63820111174f0424c1cac78f4a35d0ca
Kremio <kremio.software@gmail.com>
2016-09-09 22:53:46 +0200

    Update WebIDL binding definition.

--- binding/cppToJs.cpp
+++ binding/cppToJs.cpp
@@ -4,13 +4,15 @@
  * 'Wrappers' to interface JS and the matrix library.
  */
 
+
 /* JavaScript numbers are always stored as double precision floating point numbers, following the international IEEE 754 standard */
 class DoubleMatrix : public MatrixCRTP<double, DoubleMatrix> {
 	using MatrixCRTP<double, DoubleMatrix>::MatrixCRTP;
 
 	public:
-		static DoubleMatrix identity(const std::size_t& rows, const std::size_t& columns) {
-			return DoubleMatrix( rows, columns, Matrix<double>::identity( rows, columns, 0, 1 ).getValues() );
+
+		static DoubleMatrix getIdentity(const std::size_t& rows, const std::size_t& columns) {
+			return identity( rows, columns, 0, 1 );
 		}
 
 		DoubleMatrix(): MatrixCRTP<double, DoubleMatrix>(0, 0, 0, 1){}
@@ -22,8 +24,9 @@ class DoubleMatrix : public MatrixCRTP<double, DoubleMatrix> {
 		DoubleMatrix(std::size_t rows, std::size_t columns, double* _values) : MatrixCRTP<double, DoubleMatrix>(rows, columns, 0, 1, _values) {}
 
 		DoubleMatrix scalarMul(double scalar) { return DoubleMatrix( m, n, (*this * scalar).getValues() ); }
-		DoubleMatrix matrixMul(DoubleMatrix matrix) { return DoubleMatrix( m, matrix.getColumnsCount(), (*this * matrix).getValues() ); };
+		DoubleMatrix matrixMul(DoubleMatrix matrix) { return DoubleMatrix( m, matrix.getColumnsCount(), (*this * matrix).getValues() ); }
 
+		bool equal(DoubleMatrix matrix){ return *this == matrix; }
 
 		const char* asString(){
 			return toString().c_str();
